e4897d99b74644c76900a56f2e265376a610b206
Julian Möwenmann <moewenmann@protonmail.ch>
2024-06-11 00:19:36 +0200

    run func

--- ForwardProjection.java
+++ ForwardProjection.java
@@ -1,9 +1,21 @@
 import ij.ImagePlus;
+import ij.process.ImageProcessor;
 
 public class ForwardProjection implements PlugInFilter {
 
 	// return DOES_8G - specify plugin works w 8-bit grayscale img
 	public int setup(String arg, ImagePlus imp) {
 		return DOES_8G;
+	}
+
+	public void run(ImageProcessor ip) {
+		int width = ip.getWidth();
+		int height = ip.getHeight();
+		double[] xValues = new double[width];
+
+		for (int i = 0; i > width; i++) {
+			xValues[i] = i;
+		}
+
 	}
 }
